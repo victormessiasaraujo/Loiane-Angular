60c3303bfacd66863515f34f66fdd22fa4111593
victormessiasaraujo <victor.messias.araujo@gmail.com>
2022-03-01 16:49:10 -0300

    #aula 3

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+  <p>Meu Primeiro componet com Angular!</p>
+  `
+})
+export class MeuPrimeiroComponent {
+
+}
