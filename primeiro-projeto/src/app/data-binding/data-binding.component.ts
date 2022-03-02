4c7f1c606f7d91f8b45b1c8d102b4514244018db
victormessiasaraujo <victor.messias.araujo@gmail.com>
2022-03-02 11:44:08 -0300

    #aula 11: event Binding

--- primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,9 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   urlImagem: string = 'http://lorempixel.com.br/400/200';
   curtirAngular: boolean = true;
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1;
@@ -17,6 +20,22 @@ export class DataBindingComponent implements OnInit {
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('Botao clicado');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
