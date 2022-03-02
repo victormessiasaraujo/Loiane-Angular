5e88c4a9cbfacd01deb94a0d55e8451a6a5115a3
victormessiasaraujo <victor.messias.araujo@gmail.com>
2022-03-01 21:49:22 -0300

    #aula6: Templates

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ["C#", "Angular", "Ext JS", ".NET"];
+
+  constructor() {
+    this.nomePortal = 'http:loiane.training';
+  }
 
   ngOnInit(): void {
   }
